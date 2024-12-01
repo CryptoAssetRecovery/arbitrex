01a3a765682faa8e2405978bdae0820627b08a4c
Charles Brooks <charles@cryptoassetrecovery.com>
2024-11-30 22:54:47 -0500

    Update

--- arbitrex/urls.py
+++ arbitrex/urls.py
@@ -4,6 +4,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+import os
+
+def debug_static(request):
+    static_root = settings.STATIC_ROOT
+    static_dirs = settings.STATICFILES_DIRS
+    
+    content = f"""
+    STATIC_ROOT: {static_root}
+    Files in STATIC_ROOT: {os.listdir(static_root) if os.path.exists(static_root) else 'Directory does not exist'}
+    
+    STATICFILES_DIRS: {static_dirs}
+    Files in first STATICFILES_DIR: {os.listdir(static_dirs[0]) if static_dirs and os.path.exists(static_dirs[0]) else 'Directory does not exist'}
+    """
+    
+    return HttpResponse(content, content_type='text/plain')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +28,7 @@ urlpatterns = [
     path('backtesting/', include('backtesting.urls')),
     path('data/', include('data.urls')),
     path("", include("dashboard.urls")),
+    path('debug-static/', debug_static),
 ]
 
 if settings.DEBUG:
